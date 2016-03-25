a7401434595d02e1e8d946263d49a67f6a358599
qianqiangliu <qianqiangliu@hotmail.com>
2016-03-25 18:53:13 +0800

    add tea

---
+++ tea.h
@@ -0,0 +1,9 @@
+#ifndef _TEA_H_
+#define _TEA_H_
+
+#include <stdint.h>
+
+int tea_encrypt(uint8_t *src, int size_src, uint8_t *key);
+int tea_decrypt(uint8_t *src, int size_src, uint8_t *key);
+
+#endif /* _TEA_H_ */
