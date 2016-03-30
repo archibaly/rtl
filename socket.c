81e57a33624a98b8315a0376fd12bf166fd4550b
qianqiangliu <qianqiangliu@hotmail.com>
2016-03-30 10:56:48 +0800

    add socket interfaces

--- socket.c
+++ socket.c
@@ -28,7 +28,6 @@ void socket_set_non_blocking(int sockfd)
 	}
 }
 
-
 static void socket_reuse_endpoint(int sockfd)
 {
 	int reuse = 1;
@@ -42,8 +41,7 @@ static void socket_reuse_endpoint(int sockfd)
 	}
 }
 
-
-int socket_create(void)
+int socket_create(int type)
 {
 	int sockfd;
 	/*
@@ -51,14 +49,14 @@ int socket_create(void)
 	 * through a connection already closed by the client
 	 */
 	signal(SIGPIPE, SIG_IGN);
-	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+
+	if ((sockfd = socket(AF_INET, type, 0)) < 0) {
 		ERROR("socket()");
 		abort();
 	}
 	socket_reuse_endpoint(sockfd);
 	return sockfd;
 }
-
 
 void socket_bind(int sockfd, unsigned short port)
 {
@@ -74,11 +72,112 @@ void socket_bind(int sockfd, unsigned short port)
 	}
 }
 
-
 void socket_start_listening(int sockfd)
 {
 	if (listen(sockfd, SOMAXCONN) == -1) {
 		ERROR("listen()");
 		abort();
 	}
+}
+
+/**
+ * get_ip - get ip by hostname
+ * @hostname: in
+ * @ip: out
+ */
+static int get_ip(const char *hostname, char *ip)
+{
+	struct hostent *ht;
+	ht = gethostbyname(hostname);
+
+	if (ht == NULL)
+		return -1;
+
+	if (inet_ntop(AF_INET, ht->h_addr_list[0], ip, INET_ADDRSTRLEN) == NULL)
+		return -1;
+
+	return 0;
+}
+
+/**
+ * socket_connect - connect to tcp server
+ * @host: hostname or ip of the server
+ */
+int socket_connect(const char *host, host_type_t type, uint16_t port)
+{
+	char ip[INET_ADDRSTRLEN];
+	int sockfd;
+	struct sockaddr_in server_addr;
+
+	if (type == HOST_IPV4) {
+		strcpy(ip, host);
+	} else if (type == HOST_DOMAIN) {
+		if (get_ip(host, ip) < 0) {
+			return -1;
+		}
+	} else {
+		return -1;
+	}
+
+	sockfd = socket_create(TCP);
+
+	memset(&server_addr, 0, sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(port);
+	if (inet_pton(AF_INET, ip, &server_addr.sin_addr) < 0) {
+		return -1;
+	}
+
+	if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+		return -1;
+
+	return sockfd;
+}
+
+void tcp_server_init(uint16_t port)
+{
+	int sockfd;
+
+	sockfd = socket_create(TCP);
+	socket_bind(sockfd, port);
+	socket_start_listening(sockfd);
+}
+
+int socket_recv(int sockfd, void *buff, int size)
+{
+	int n;
+
+	for (;;) {
+		if ((n = recv(sockfd, buff, size, 0)) < 0) {
+			if (errno == EINTR)
+				continue;
+			else
+				return -1;
+		} else {
+			return n;
+		}
+	}
+}
+
+int socket_send(int sockfd, const void *buff, int size)
+{
+	int nleft;
+	int nsent;
+	const char *ptr;
+
+	ptr = buff;
+	nleft = size;
+
+	while (nleft > 0) {
+		if ((nsent = send(sockfd, ptr, nleft, 0)) <= 0) {
+			if (nsent < 0 && errno == EINTR)
+				nsent = 0;		/* and call write() again */
+			else
+				return -1;		/* error */
+		}
+
+		nleft -= nsent;
+		ptr   += nsent;
+	}
+	return size;
 }
