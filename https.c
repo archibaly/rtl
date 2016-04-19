beeec9b56427d689a080bf425f5ee5517312a3f9
qianqiangliu <qianqiangliu@hotmail.com>
2016-04-19 17:34:57 +0800

    add http_send_request()

--- https.c
+++ https.c
@@ -5,60 +5,66 @@
 #include <openssl/rand.h>
 #include <openssl/crypto.h>
 
-#include "http.h"
 #include "https.h"
 #include "socket.h"
 
 /*
+ * @type: GET or POST
  * @host: the remote host's name or ip
  * @port: the remote host port
  * @path: the path to get
  * @resp: response buffer
  * @len: the length of response buffer
  */
-int https_send_get(const char *host, uint16_t port, const char *path, char *resp, int len)
+int https_send_request(enum request type, const char *host, uint16_t port, const char *path, char *resp, int len)
 {
 	char header[512];
+	int header_len;
 
-	int header_len = http_build_get_header(host, path, header);
+	if (type == GET)
+		header_len = http_build_get_header(host, path, header);
+	else if (type == POST)
+		header_len = http_build_post_header(host, path, header);
+	else
+		return -1;
 
     SSL_library_init();
     SSL_load_error_strings();
     SSL_CTX *ctx = SSL_CTX_new(SSLv23_client_method());
-    if (ctx == NULL) {
+    if (ctx == NULL)
         return -1;
-    }
 
+	int ret = 0;
 	SSL *ssl = SSL_new(ctx);
     if (ssl == NULL) {
-        return -1;
-    }
+		ret = -1;
+		goto out;
+	}
 	
 	int sockfd = socket_connect(host, port);
-	if (sockfd < 0)
-		return -1;
+	if (sockfd < 0) {
+		ret = -1;
+		goto out;
+	}
 
-    int ret = SSL_set_fd(ssl, sockfd);
-    if (ret == 0) {
-        return -1;
-    }
+    if (SSL_set_fd(ssl, sockfd) == 0) {
+		ret = -1;
+		goto out;
+	}
 
-    ret = SSL_connect(ssl);
-    if (ret != 1) {
-        return -1;
+    if (SSL_connect(ssl) != 1) {
+		ret = -1;
+		goto out;
     }
 
 	int nleft = header_len;
 	int nwritten;
 	char *ptr = header;
 	while (nleft > 0) {
-		if ((nwritten = SSL_write(ssl, ptr, nleft)) < 0) {
-			/* has written all data */
-			break;
-		} else if (nwritten == 0) {
-			return -1;	/* error */
+		if ((nwritten = SSL_write(ssl, ptr, nleft)) <= 0) {
+			ret = -1;
+			goto out;
 		}
-
 		nleft -= nwritten;
 		ptr   += nwritten;
 	}
@@ -71,20 +77,23 @@ int https_send_get(const char *host, uint16_t port, const char *path, char *resp
 		if (nread == 0) {	/* receive done */
 			break;
 		} else if (nread < 0) {
-			return -1;
+			ret = -1;
+			goto out;
 		}
 		len -= nread;
 		ptr += nread;
-		if (len < 0)
-			return -1;
+		if (len < 0) {	/* the buffer of resp is not enough */
+			ret = -1;
+			goto out;
+		}
 	}
-	nread = ptr - resp;
-	ptr[nread] = '\0';
+	ret = ptr - resp;
 
+out:
     SSL_shutdown(ssl);
     close(sockfd);
     SSL_free(ssl);
     SSL_CTX_free(ctx);
 
-	return nread;
+	return ret;
 }
