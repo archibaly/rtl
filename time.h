41ee281eedd47ac65ee6e6a6f2a1e250c1d6433b
qianqiangliu <qianqiangliu@hotmail.com>
2016-08-09 13:56:58 +0800

    update time

--- time.h
+++ time.h
@@ -1,6 +1,17 @@
 #ifndef _TIME_H_
 #define _TIME_H_
 
+struct time {
+	int year;
+	int mon;
+	int mday;
+	int wday;
+	int hour;
+	int min;
+	int sec;
+};
+
+int time_get(struct time *t);
 int time_fmt(char *s, size_t size, const char *fmt);
 
 #endif /* _TIME_H_ */
