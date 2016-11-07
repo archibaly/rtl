89fe87a52034475ef4b922211e219f5f4eb1e479
qianqiangliu <qianqiangliu@hotmail.com>
2016-11-07 10:34:29 +0800

    fix http.c bug

--- test/http/http.c
+++ test/http/http.c
@@ -37,11 +37,9 @@ int main(int argc, char **argv)
 
 	if (strcmp(argv[1], "http") == 0) {
 		rtl_http_recv_response(sockfd, (uint8_t *)resp, sizeof(resp));
-		close(sockfd);
 		/* rtl_http_save_body_to_file(sockfd, "body"); */
 	} else {
 		rtl_https_recv_response(&ssl, (uint8_t *)resp, sizeof(resp));
-		rtl_https_end_request(&ssl);
 		/* rtl_https_save_body_to_file(&ssl, "body"); */
 	}
 
